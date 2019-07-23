99d644833e1a9d8ef45881ccf78bced0bd223cf8
Wes Jonke <wesjonke@gmail.com>
2019-07-23 15:45:22 -0400

    initial project structure set up

---
+++ reducer-todo/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+
+
+  return (
+
+
+    <div className="App">
+
+      <h1>coming soon</h1>
+      
+    </div>
+  );
+}
+
+export default App;
